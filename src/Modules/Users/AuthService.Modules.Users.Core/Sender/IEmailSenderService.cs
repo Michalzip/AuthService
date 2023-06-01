5552d36e22482187a148612d49bee5ef3aba17db
Michał Potoczny <michal@MacBook-Air-Micha.local>
2023-06-01 18:45:40 +0200

    delete unused class

--- src/Modules/Users/AuthService.Modules.Users.Core/Sender/IEmailSenderService.cs
+++
@@ -1,9 +0,0 @@
-using AuthService.Modules.Users.Core.ValueObjects.EmailStatus;
-
-namespace AuthService.Modules.Core.Sender
-{
-    public interface IEmailSenderService
-    {
-        Task<EmailStatus> SendEmailAsync(string email, string subject, string plainTextContent, string htmlContent);
-    }
-}
